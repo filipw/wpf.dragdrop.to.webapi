6fc2a2a1c3803b62957c5ba8f3ce3d9fd32740f6
Filip W <wojcieszyn@gmail.com>
2012-06-17 23:05:39 +0200

    fixed build

---
+++ WpfApplication1/FileDesc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class FileDesc
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public long size { get; set; }
+    }
+}
